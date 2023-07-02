bd3971945c6e9c83400817e07c8c054ea8082973
arijit bhattacharyya <arijitron@gmail.com>
2023-07-02 19:06:20 +0000

    indexed

--- week1/index_products.py
+++ week1/index_products.py
@@ -85,7 +85,15 @@ def get_opensearch():
     port = 9200
     auth = ('admin', 'admin')
     #### Step 2.a: Create a connection to OpenSearch
-    client = None
+    client = OpenSearch(
+        hosts=[{'host': host, 'port': port}],
+        http_compress=True,  # enables gzip compression for request bodies
+        http_auth=auth,
+        use_ssl=True,
+        verify_certs=False,
+        ssl_assert_hostname=False,
+        ssl_show_warn=False,
+    )
     return client
 
 
@@ -106,10 +114,19 @@ def index_file(file, index_name):
         #print(doc)
         if 'productId' not in doc or len(doc['productId']) == 0:
             continue
-        #### Step 2.b: Create a valid OpenSearch Doc and bulk index 2000 docs at a time
-        the_doc = None
-        docs.append(the_doc)
-
+        if doc["name"] and len(doc["name"]):
+            doc["suggest"] = doc["name"] 
+            doc["canonical"] = doc["name"][0].strip().title().encode("ascii", "ignore").decode()
+        docs.append({'_index': index_name, '_id':doc['sku'][0], '_source' : doc})
+        
+        docs_indexed += 1
+        if docs_indexed % 200 == 0:
+            bulk(client, docs, request_timeout=60)
+            
+            docs = []
+    if len(docs) > 0:
+        bulk(client, docs, request_timeout=60)
+        logger.info(f'{docs_indexed} documents indexed')
     return docs_indexed
 
 @click.command()
